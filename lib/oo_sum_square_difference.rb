d09262b2d2b8d5030aa41194917608e52769b9fd
github username <github email address>
2017-05-01 19:06:42 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,21 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+
+  attr_accessor :range, :difference
+
+  def initialize(limit)
+    @range = (1..limit).to_a
+    @difference = find_difference
+  end
+
+  def find_difference
+    squared_sum - sum_squares
+  end
+
+  def sum_squares
+    @range.map {|el| el ** 2}.reduce(:+)
+  end
+
+  def squared_sum
+    @range.reduce(:+) ** 2
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,12 @@
-# Implement your procedural solution here!
+def sum_square_difference(limit)
+  array = (1..limit).to_a
+  squared_sum(array) - sum_squares(array)
+end
+
+def sum_squares(array)
+  array.map {|el| el ** 2}.reduce(:+)
+end
+
+def squared_sum(array)
+  array.reduce(:+) ** 2
+end
