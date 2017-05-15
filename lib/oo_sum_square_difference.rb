59c1669460a4fcba13946be69fcd4503cd99c7ba
fakebenjay <fakebenjay@gmail.com>
2017-05-15 15:38:32 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,24 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(number)
+    counter = number
+    array = []
+
+    until counter == 0
+      array << counter
+      counter -= 1
+    end
+
+    array_squared = array.map do |a|
+      a * a
+    end
+
+    @sum_squares = array_squared.inject {|sum, i| sum + i}
+
+    array_sum = array.inject {|sum, i| sum + i}
+    @square_sum = array_sum * array_sum
+  end
+
+  def difference
+    @square_sum - @sum_squares
+  end
+end
